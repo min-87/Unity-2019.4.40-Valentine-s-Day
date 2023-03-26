c16eee46af1c2438962f68d48c927323d2792260
min-87 <33282348+min-87@users.noreply.github.com>
2023-03-26 16:07:56 +0300

    Unity 2019.4.40 Valentine's Day
    Потрібно ловити тільки червоні сердця. Якщо зловиш чорне, то Game over

---
+++ New Unity Project/Assets/Script/CamRay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CamRay : MonoBehaviour
+{
+    private Camera myCam;
+    private TMP_Text scoreText;
+    private int score;
+    void Start()
+    {
+        myCam = GetComponent<Camera>();
+        scoreText = GameObject.Find("ScoreText").GetComponent<TMP_Text>();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if(Physics.Raycast(ray, out hit)){
+                GameObject temp = hit.collider.gameObject;
+                if(temp.CompareTag("Heart")){
+                    Destroy(temp);
+                    score++;
+                    scoreText.text = $"Score: {score}";
+                }else if(temp.CompareTag("BedHeart")){
+                    Time.timeScale = 0f;
+                }
+            }
+        }
+    }
+}
---
+++ New Unity Project/Assets/Script/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private float timer;
+    [SerializeField] private GameObject heartPrefab;
+    [SerializeField] private GameObject badHeartPrefab;
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn(){
+        while(true){
+            if(Random.Range(0, 100) > 25){
+                GameObject temp = Instantiate(heartPrefab, transform.position, Quaternion.identity);
+                Destroy(temp, 5f);
+            }else{
+                GameObject temp = Instantiate(badHeartPrefab, transform.position, Quaternion.identity);
+                Destroy(temp, 5f);
+            }
+            yield return new WaitForSeconds(timer);
+        }
+    }
+}
